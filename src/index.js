0b9dc9334a6c9ebc2e8570de85ae20da23d71339
NerlyTon <nerlyton@gmail.com>
2021-04-07 22:54:43 -0500

    removed cosole.log

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const nav = document.querySelector('.navbar')
 
 window.addEventListener('scroll', function(e) {
-    console.log(this.scrollY)
     if(this.scrollY > 20) {
         nav.classList.add('sticky')
     } else {
